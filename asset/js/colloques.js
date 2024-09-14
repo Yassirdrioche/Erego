a6c2daf5bbd9d86172b9b9da4863b90e14785918
Yassirdrioche <yassirdroiche@gmail.com>
2024-09-14 16:06:19 +0100

    nav fixed and  alignement

--- asset/js/colloques.js
+++ asset/js/colloques.js
@@ -1,10 +1,7 @@
 const dropDown = document.querySelector("#drop-down");
 const dropItems = document.querySelector("#drop-items");
+const arrowUp = document.querySelector("#angledown");
 dropDown.addEventListener("click", () => {
+  arrowUp.classList.toggle("rotate");
   dropItems.classList.toggle("show");
-});
-const seminaireDrop = document.getElementById("seminairedrop");
-const seminaireItems = document.getElementById("seminaireitme");
-seminaireDrop.addEventListener("click", () => {
-  seminaireItems.classList.toggle("show");
 });
