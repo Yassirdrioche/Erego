c1cf62889e7542f5fcd62d6e3fc7319b20c29672
Yassirdrioche <yassirdroiche@gmail.com>
2024-09-21 04:38:23 +0100

    btn modified

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,3 +2,7 @@ const btnUp = document.getElementById("btnUp");
 btnUp.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+window.addEventListener("scroll", () => {
+  btnUp.style.opacity = window.scrollY > 400 ? "1" : "0";
+  btnUp.disabled = window.scrollY > 400 ? false : true;
+});
