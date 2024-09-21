3c7d4885471f26640c85201c26b66ef361a357cd
Yassirdrioche <yassirdroiche@gmail.com>
2024-09-21 13:24:28 +0100

    tde btn added

--- asset/js/cirlog.js
+++ asset/js/cirlog.js
@@ -1,6 +1,7 @@
 const dropDown = document.querySelector("#drop-down");
 const dropItems = document.querySelector("#drop-items");
 const arrowUp = document.querySelector("#angledown");
+
 dropDown.addEventListener("click", () => {
   arrowUp.classList.toggle("rotate");
   dropItems.classList.toggle("show");
